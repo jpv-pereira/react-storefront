3c7d9272206948c05d482a706bad4b2c47d31ac8
jpv-pereira <jpvp.pedro9@gmail.com>
2024-09-24 01:53:28 +0000

    Add a basic Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
+import ReactStoreLogo from "../../assets/reactstore-logo.png";
+
+function Logo({ src, alt }) {
+  return <img className="max-h16 sm:max-h-24" src={src} alt={alt} />;
+}
+
 function Header() {
   return (
     <section>
       <header>
-        <button className="Header">Hello</button>
+        <div className="p-4 sm:p-8 flex basis-auto flex-auto space-x-8">
+          <Logo src={ReactStoreLogo} alt="React Store Logo" />
+          <button className="text-5xl">Buy</button>
+        </div>
       </header>
     </section>
   );
