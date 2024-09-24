d06084a41de11900895d5e6abbc3256b6322e9aa
jpv-pereira <jpvp.pedro9@gmail.com>
2024-09-24 00:25:20 +0000

    add Tailwind CSS for styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <section>
+      <header>
+        <button className="Header">Hello</button>
+      </header>
+    </section>
+  );
+}
+
+export default Header;
