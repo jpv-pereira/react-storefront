d11da8e41f6f416454a8754e950a749ca0ccf4c8
jpv-pereira <jpvp.pedro9@gmail.com>
2024-09-27 13:44:53 +0000

    Header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,16 @@
-import ReactStoreLogo from "../../assets/reactstore-logo.png";
-
-function Logo({ src, alt }) {
-  return <img className="max-h16 sm:max-h-24" src={src} alt={alt} />;
-}
+import Divider from "./Divider";
 
 function Header() {
   return (
     <section>
       <header>
-        <div className="p-4 sm:p-8 flex basis-auto flex-auto space-x-8">
-          <Logo src={ReactStoreLogo} alt="React Store Logo" />
-          <button className="text-5xl">Buy</button>
+        <Divider />
+        <div className="p-2 sm:p-3 space-x-8 flex justify-center">
+          <button className="text-2xl font-semibold tracking-wide text-gray-600 hover:text-black transition duration-300">
+            Shop
+          </button>
         </div>
+        <Divider />
       </header>
     </section>
   );
