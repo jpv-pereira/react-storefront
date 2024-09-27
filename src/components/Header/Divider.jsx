6fb01c3d787972956b55c8389cdfce1930bad702
jpv-pereira <jpvp.pedro9@gmail.com>
2024-09-27 13:45:23 +0000

    Styling updates to the page

---
+++ src/components/Header/Divider.jsx
@@ -0,0 +1,9 @@
+function Divider() {
+  return (
+    <>
+      <div className="p-px ml-44 mr-44 space-x-8 bg-gradient-to-r from-transparent via-black to-transparent"></div>
+    </>
+  );
+}
+
+export default Divider;
