db43bc3f016bb7aedfd1347c57b6bd8119455aac
jpv-pereira <jpvp.pedro9@gmail.com>
2024-09-29 01:22:21 +0000

    Move store logo to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import Divider from "./Divider";
+import ReactStoreLogo from "../../assets/reactstore-logo.png";
 
 function Header() {
   return (
     <section>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 p-1">
+        <img
+          className="object-contain max-h6 sm:max-h-12"
+          src={ReactStoreLogo}
+          alt="React Store Logo"
+        />
         <a className="btn btn-ghost text-xl">Shop</a>
       </div>
     </section>
