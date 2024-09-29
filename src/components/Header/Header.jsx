7e967e16da90b3d606b5d64659ab2e14430a2ff4
jpv-pereira <jpvp.pedro9@gmail.com>
2024-09-29 01:15:38 +0000

    Restyle Header with DaisyUI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,9 @@ import Divider from "./Divider";
 function Header() {
   return (
     <section>
-      <header>
-        <Divider />
-        <div className="p-2 sm:p-3 space-x-8 flex justify-center">
-          <button className="btn">Shop</button>
-        </div>
-        <Divider />
-      </header>
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost text-xl">Shop</a>
+      </div>
     </section>
   );
 }
