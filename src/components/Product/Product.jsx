78f266164cd929f8af6671824630f3496259d69b
jpv-pereira <jpvp.pedro9@gmail.com>
2024-10-01 23:27:22 +0000

    Remove Product model as it is unnecessary

--- src/components/Product/Product.jsx
+++
@@ -1,9 +0,0 @@
-class Product {
-  constructor(id, name, description, img, price) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.img = img;
-    this.price = price;
-  }
-}
